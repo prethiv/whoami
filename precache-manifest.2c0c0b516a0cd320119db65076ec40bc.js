8fe19125e0a5b85b30766e1e92e5ce6518be3c70
Prethiv <prethiv191@gmail.com>
2021-09-08 18:57:16 +0530

    Updates

---
+++ precache-manifest.2c0c0b516a0cd320119db65076ec40bc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89545e23c6455d5e04fc33bc39ace03",
+    "url": "/whoami/index.html"
+  },
+  {
+    "revision": "36b2f563192e6c188113",
+    "url": "/whoami/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "c2b5786e10b82a523a1f",
+    "url": "/whoami/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36b2f563192e6c188113",
+    "url": "/whoami/static/js/2.d9d70c5e.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/whoami/static/js/2.d9d70c5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2b5786e10b82a523a1f",
+    "url": "/whoami/static/js/main.6e6d412c.chunk.js"
+  },
+  {
+    "revision": "fd0ed12f1d58b4369934",
+    "url": "/whoami/static/js/runtime-main.c320ed15.js"
+  },
+  {
+    "revision": "d6dc6f103f4927e073f7f0acfc5a3b7e",
+    "url": "/whoami/static/media/chash.d6dc6f10.jfif"
+  },
+  {
+    "revision": "9c359cb3f63bb16bcaf7b339ac545323",
+    "url": "/whoami/static/media/logo.9c359cb3.svg"
+  },
+  {
+    "revision": "9c359cb3f63bb16bcaf7b339ac545323",
+    "url": "/whoami/static/media/profilepic.9c359cb3.jpg"
+  },
+  {
+    "revision": "970b96aa4e291958bfdf52dba670c7da",
+    "url": "/whoami/static/media/scss.970b96aa.jfif"
+  },
+  {
+    "revision": "a0c446d5c5af299bd68e2c72d1c77061",
+    "url": "/whoami/static/media/webdev.a0c446d5.jfif"
+  }
+]);
